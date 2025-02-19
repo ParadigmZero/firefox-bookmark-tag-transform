eb68dfaf2976ccc0634f4e22f9dfaf765a17f05b
ParadigmZero <josephrcalton@gmail.com>
2025-02-19 13:01:43 +0000

    fix issues with last published package, and give correct version number and republish

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 #! /usr/bin/env node
-import transformfirefoxbookmarktags from './aux.js';
+import transformfirefoxbookmarktags from './transform-tags.js';
 transformfirefoxbookmarktags(process.argv[2]);
